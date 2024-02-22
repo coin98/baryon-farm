cd28b2744c0f946190913ef269b91bd08f18bbce
Khang Nguyen Truong <khangnt@coin98.finance>
2024-02-22 16:42:05 +0700

    Refactor code to improve performance and readability

---
+++ contracts/libraries/SafeMath.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+library SafeMath {
+    function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x, "SafeMath: ds-math-add-overflow");
+    }
+
+    function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x, "SafeMath: ds-math-sub-underflow");
+    }
+
+    function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(
+            y == 0 || (z = x * y) / y == x,
+            "SafeMath: ds-math-mul-overflow"
+        );
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+}
---
+++ contracts/libraries/TimeLock.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import './Ownable.sol';
+
+/**
+ * @dev Provide mechanism for Time Locking, Owner of contract can unlock this contract, after locking time
+ * owner can execute special function and then contract will be lock again.
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+contract TimeLock is Ownable {
+    uint256 private _lockTime;
+
+    mapping(bytes4 => bool) _isUnlock;
+    mapping(bytes4 => uint256) _unlockAts;
+
+    event Unlock(bytes4 _functionSign, uint256 _timeUnlock);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial lock time.
+     */
+    constructor(uint256 lockTime) {
+        _lockTime = lockTime;
+    }
+
+    /**
+     * @dev Returns contract is unlock.
+     */
+    function isUnlock(bytes4 _functionSign) public view virtual returns (bool) {
+        return
+            _isUnlock[_functionSign] &&
+            (_unlockAts[_functionSign] + _lockTime) <= block.timestamp;
+    }
+
+    /**
+     * @dev Throws if contract is lock, after execute function contract will be lock again.
+     */
+    modifier whenUnlock() {
+        require(isUnlock(msg.sig), "LockSchedule: contract is locked");
+        _;
+        _isUnlock[msg.sig] = false;
+    }
+
+    /**
+     * @dev Unlock contract, contract state Lock -> Pending -> Unlock -> Lock.
+     */
+    function unlock(bytes4 _functionSign) external onlyOwner {
+        _isUnlock[_functionSign] = true;
+        _unlockAts[_functionSign] = block.timestamp;
+
+        emit Unlock(_functionSign, block.timestamp);
+    }
+}
