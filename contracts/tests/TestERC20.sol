d409703213c2070c78dd4e8f74fa3bb0bdb144c0
Khang Nguyen Truong <khangnt@coin98.finance>
2024-02-26 00:15:59 +0700

    add test contracts

---
+++ contracts/tests/TestERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+    constructor(uint256 initialSupply) ERC20("TestERC20", "TERC20") {
+        _mint(msg.sender, initialSupply);
+    }
+}
